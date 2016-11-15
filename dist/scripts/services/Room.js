f0176571178501c0556e82212ce226925af72b17
Jason Kincaid <jasonkincaid01@gmail.com>
2016-11-15 15:43:55 -0600

    user story 4 complete

--- dist/scripts/services/Room.js
+++ dist/scripts/services/Room.js
@@ -2,15 +2,26 @@
 	function Room($firebaseArray, $uibModal) {
 		var ref = firebase.database().ref().child('rooms');
 		var rooms = $firebaseArray(ref);
+		var messageRef = firebase.database().ref().child('messages');
+
+		//Create new chatroom by updating $firebaseArray
 
 		var createChatRoom = function(room) {
 			rooms.$add(room);
 		};
 
+		//Get messages from current room based on roomId
+
+		var getMessages = function(roomId){
+			var currentRoomMessages = messageRef.orderByChild("roomId").equalTo(roomId);
+			return $firebaseArray(currentRoomMessages);
+		};
+
 		
 		return {
 			all: rooms,
-			createChatRoom: createChatRoom
+			createChatRoom: createChatRoom,
+			getMessages: getMessages
 		};
 	}
 
